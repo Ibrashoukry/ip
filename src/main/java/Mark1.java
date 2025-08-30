0ea6582e2855a23b6cfae833af44533c1be10bc7
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2025-08-31 00:44:26 +0800

    Add user input and echo input

--- src/main/java/Mark1.java
+++ src/main/java/Mark1.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Mark1 {
     public static void main(String[] args) {
-        String lineBreak = "_________________________\n";
-        System.out.println(lineBreak + "Hello I'm Mark I\n" + "What can I do for you?");
-        System.out.println(lineBreak + "Bye. See you again!\n" + lineBreak);
+        String lineBreak = "~~~~~~~~~~~~~~~~~~~~~~\n";
+        System.out.println(lineBreak + "Hello I'm Mark I\n" + "Alssalam Alaikum. I'm here to assist you *_*\n" + lineBreak);
+
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine(); // initial request
+
+        // do tasks if user does not want to exit
+        while (!command.contains("bye")) {
+            System.out.println("\n" + lineBreak + command + "\n" + lineBreak);
+            command = in.nextLine();
+        }
+
+        System.out.println(lineBreak + "Bye. Come back for more help ;)\n" + lineBreak);
+        in.close();
+
+
     }
 }
