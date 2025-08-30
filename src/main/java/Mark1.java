7d14372627c3ea9ef5e10d8d37fd647ac9c8eee0
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2025-08-31 00:05:59 +0800

    Add greeting

---
+++ src/main/java/Mark1.java
@@ -0,0 +1,7 @@
+public class Mark1 {
+    public static void main(String[] args) {
+        String lineBreak = "_________________________\n";
+        System.out.println(lineBreak + "Hello I'm Mark I\n" + "What can I do for you?");
+        System.out.println(lineBreak + "Bye. See you again!\n" + lineBreak);
+    }
+}
