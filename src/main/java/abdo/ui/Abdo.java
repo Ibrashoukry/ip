54ed598886ef291bedaada26ef29213d49daf8ea
Ibrashoukry <ibrahim.m.shoukry@gmail.com>
2025-10-03 20:35:55 +0800

    Add saving feature of task list to ./data/tasks.txt directory

--- src/main/java/abdo/ui/Abdo.java
+++ src/main/java/abdo/ui/Abdo.java
@@ -7,7 +7,11 @@ import abdo.process.Printer;
 import abdo.process.Task;
 import abdo.process.Todo;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class Abdo {
 
@@ -59,12 +63,98 @@ public class Abdo {
         return task;
     }
 
-    public static void main(String[] args) {
+    // Makes changes to the file whenever an addition or deletion is done
+    public static void updateFile(File file, Task[] tasks, int tasksLen) throws IOException {
+
+        FileWriter fw = new FileWriter(file);
+        Task task;
+        String doneIndicator;
+
+        for (int i = 0; i < tasksLen; i++) {
+
+            task = tasks[i];
+            doneIndicator = (task.getIsDone()) ? "1" : "0";
+
+            if (task instanceof Todo) {
+                fw.write("T|" + doneIndicator + "|" + task.getDescription() + System.lineSeparator());
+            } else if (task instanceof Deadline) {
+                fw.write("D|" + doneIndicator + "|" + task.getDescription() +
+                        "|" + ((Deadline) task).getBy() + System.lineSeparator());
+            } else if (task instanceof Event) {
+                fw.write("E|" + doneIndicator + "|" + task.getDescription() +
+                        "|" + ((Event) task).getFrom() + "|" + ((Event) task).getTo() + System.lineSeparator());
+            }
+        }
+
+        fw.close();
+    }
+
+    // Populates the tasks list if it is the beginning of the session
+    public static int populateList(File file, Task[] tasks, int nextTask) throws FileNotFoundException {
+
+        Scanner s = new Scanner(file);
+        String[] parsedLine;
+        Task task;
+        Printer printer = new Printer();
+        boolean isDone;
+
+        while(s.hasNext()) {
+            parsedLine = s.nextLine().split("\\|");
+            try {
+                switch (parsedLine[0]) {
+                case "T":
+                    task = new Todo(parsedLine[2]);
+                    break;
+                case "D":
+                    task = new Deadline(parsedLine[2], parsedLine[3]);
+                    break;
+                case "E":
+                    task = new Event(parsedLine[2], parsedLine[3], parsedLine[4]);
+                    break;
+                default:
+                    task = null;
+                    break;
+                }
+
+                if (task != null) {
+                    isDone = parsedLine[1].equals("1");
+                    task.setDone(isDone);
+                    tasks[nextTask] = task;
+                    nextTask++;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(printer.lineBreak + "Invalid format in tasks file" + printer.lineBreak);
+            }
+        }
+
+        s.close();
+
+        return nextTask;
+    }
+
+    public static void main(String[] args) throws IOException {
 
         Printer printer = new Printer();
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[MAX_TASKS];
         int nextTask = 0;
+
+        File file = new File("./data/tasks.txt");
+        File parentDir = file.getParentFile();
+
+        // creates new parent folder if directory doesn't exist
+        if (!parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        // populate list with existing tasks from file
+        if (file.length() != 0) {
+            nextTask = populateList(file, tasks, nextTask);
+        }
 
         printer.printGreeting();
 
@@ -105,6 +195,7 @@ public class Abdo {
                     tasks[nextTask] = processTask(command, parsedCommand[0]);
                     printer.printAddTask(tasks[nextTask], nextTask + 1);
                     nextTask++;
+                    updateFile(file, tasks, nextTask);
                 } catch (AbdoException e) {
                     System.out.print(e.getMessage());
                 }
